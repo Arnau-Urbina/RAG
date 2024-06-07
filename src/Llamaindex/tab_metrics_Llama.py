59ec93b919668250cb82f43114df2a6282edfdd5
Arnau-Urbina <163839495+Arnau-Urbina@users.noreply.github.com>
2024-06-07 14:10:15 +0200

    Add files via upload

---
+++ src/Llamaindex/tab_metrics_Llama.py
@@ -0,0 +1,79 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from llama_index.core.evaluation import BatchEvalRunner, CorrectnessEvaluator, FaithfulnessEvaluator, RelevancyEvaluator
+from tab_generations_Llama import llm_Llama 
+
+#def metrics_Llama(llm, qe, queries):
+#    # Initialize the evaluators
+#    correctness_evaluator = CorrectnessEvaluator(llm = llm) # Useful for measuring if the response is correct against a reference answer
+#    faithfulness_evaluator = FaithfulnessEvaluator(llm = llm) # Useful for measuring if the response is hallucinated
+#    relevancy_evaluator = RelevancyEvaluator(llm = llm) # Useful for measuring if the query is actually answered by the response
+#    runner = BatchEvalRunner(
+#    {
+#        "correctness": correctness_evaluator,
+#        "faithfulness": faithfulness_evaluator,
+#        "relevancy": relevancy_evaluator,
+#    }, show_progress = True)
+#    # Run the asynchronous evaluation
+#    eval_result = await runner.aevaluate_queries(
+#        query_engine = qe, #index.as_query_engine(),
+#        queries = [question for question in queries]
+#    )
+#
+#return eval_result
+
+
+
+### View metric evaluations 
+def plot_metrics(scores, title = 'RAG Metrics'):
+    # Create the plot
+    plt.figure(figsize=(10, 6))
+    names = list(scores.keys())
+    values = list(scores.values())
+    bars = plt.bar(names, values, color=sns.color_palette("viridis", len(names)))
+
+    # Adding the values on top of the bars
+    for bar in bars:
+        height = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width() / 2,  # x-position
+                height + 0.01,  # y-position
+                f'{height:.4f}',  # value
+                ha='center', va='bottom')
+
+    plt.ylabel('Score')
+    plt.title('RAG Metrics')
+    plt.ylim(0, 1.2)  # Setting the y-axis limit to be from 0 to 1
+    #plt.show()
+
+    # Display the plot in Streamlit
+    st.pyplot(plt.gcf())
+
+
+
+
+def tab_metric_Lla():
+    st.header("RAG Metrics")
+    st.write("Once we have made the RAG, we are going to evaluate it with different metrics from the ragas library.")
+
+    if st.button("Run", key = "Metrics_Llama"):
+        questions = st.session_state['editable_questions'] 
+        #ground_truths = st.session_state['editable_ground_truths']
+        context = st.session_state['context']  
+        answer = st.session_state['answer']
+        retriever = st.session_state['retriever_Llama']
+
+        st.write('Scores: ')
+        scores = metrics_Llama(llm_Llama, retriever, questions)
+        st.write(scores)
+        #st.write('We can see the metrics in a graph:')
+        #plot_metrics(scores = scores)
+        
+        #st.write('Finally, we show the table with the different queries and their groundtruth, answer, context and metrics.')
+        #obs = pd.DataFrame(db)
+        #met = pd.DataFrame(scores.scores)
+        #table = pd.concat([obs, met], axis=1)
+        #st.write(table)
+
---
+++ src/Llamaindex/tab_results_Llama.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+
+
+def tab_comparision():
+    st.header("Comparision")
+    st.write("In this tabe we can compare the results of a diferentes RAGs models.")
+
+    if st.button("Run", key = 'Comparision_Llama'):
+        chunk_model = st.session_state['chunk_model']
+        chunk_size = st.session_state['chunk_size']
+        overlap = st.session_state['overlap']
+        embed_model = st.session_state['embed_model']
+        retrieval_model = st.session_state['retrieval_model']
+        questions = st.session_state['editable_questions'] 
+        scores = st.session_state['scores']
+        
+        data = {
+            "Chunk Model": [chunk_model],
+            "Chunk Size": [chunk_size],
+            "Overlap": [overlap],
+            "Embed model": [embed_model],
+            "Retrieval Model": [retrieval_model],
+            "Number of queries": [len(questions)],
+            "Faithfulness": [scores['faithfulness']],
+            "Answer relevancy": [scores['answer_relevancy']],
+            "Context precision": [scores['context_precision']],
+            "Framework": ['LlamaIndex'],
+        }
+
+        table = pd.DataFrame(data)
+
+        final_data = pd.read_csv('../Data/output/ComparisionRAGs.csv')
+        merg = pd.concat([final_data, table], axis = 0)
+        st.write(merg)
+        merg.to_csv('../Data/output/ComparisionRAGs.csv', index = False)
+
